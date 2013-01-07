ca079df6bab8b9821dcf4dfdb75fa3a5bc83b420
Dan Allan <daniel.b.allan@gmail.com>
2013-01-06 19:42:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobtown::Application.config.secret_token = '655f81730f82875b849e13c584124b91c7aa416a25e5049ca58c336b5b51790567f7f4055add7a67a1bffb38540d16fecb2d7dce78011c731887af78661ddc9d'
