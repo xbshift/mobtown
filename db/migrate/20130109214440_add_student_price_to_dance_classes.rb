b948abcd473d66bae2c18c02e928d73fdaf0e8e5
Dan Allan <daniel.b.allan@gmail.com>
2013-01-09 16:52:36 -0500

    fixed student price migration

--- db/migrate/20130109214440_add_student_price_to_dance_classes.rb
+++ db/migrate/20130109214440_add_student_price_to_dance_classes.rb
@@ -1,6 +1,6 @@
 class AddStudentPriceToDanceClasses < ActiveRecord::Migration
   def change
-    add_column, :student_price, :decimal, :precision => 8, :scale => 2
-    add_column, :session_dates, :string
+    add_column :dance_classes, :student_price, :decimal, :precision => 8, :scale => 2
+    add_column :dance_classes, :session_dates, :string
   end
 end
