692291966075cdcfc7fc709948fac05981418073
Dan Allan <daniel.b.allan@gmail.com>
2013-01-09 13:33:48 -0500

    basic auth

--- app/controllers/dance_classes_controller.rb
+++ app/controllers/dance_classes_controller.rb
@@ -1,4 +1,6 @@
 class DanceClassesController < ApplicationController
+  before_filter :authenticate, :except => [:index, :show]
+
   def new
     @dance_class = DanceClass.new
  
@@ -8,22 +10,22 @@ class DanceClassesController < ApplicationController
     end
   end
 
-def create
-  @dance_class = DanceClass.new(params[:post])
+  def create
+    @dance_class = DanceClass.new(params[:post])
  
-  respond_to do |format|
-    if @dance_class.save
-      format.html  { redirect_to(@dance_class,
-                    :notice => 'Class was successfully created.') }
-      format.json  { render :json => @dance_class,
-                    :status => :created, :location => @dance_class }
-    else
-      format.html  { render :action => "new" }
-      format.json  { render :json => @dance_class.errors,
+    respond_to do |format|
+      if @dance_class.save
+        format.html  { redirect_to(@dance_class,
+                      :notice => 'Class was successfully created.') }
+        format.json  { render :json => @dance_class,
+                      :status => :created, :location => @dance_class }
+      else
+        format.html  { render :action => "new" }
+        format.json  { render :json => @dance_class.errors,
                     :status => :unprocessable_entity }
+      end
     end
   end
-end
 
   def index
     @all_dance_classes = DanceClass.all
@@ -40,33 +42,37 @@ end
       format.json  { render :json => @dance_classes }
     end
   end
-end
 
-def edit
-  @dance_class = DanceClass.find(params[:id])
-end
+  def edit
+    @dance_class = DanceClass.find(params[:id])
+  end
 
-def update
-  @dance_class= DanceClass.find(params[:id])
+  def update
+    @dance_class= DanceClass.find(params[:id])
  
-  respond_to do |format|
-    if @dance_class.update_attributes(params[:dance_class])
-      format.html  { redirect_to(@dance_class,
-                    :notice => 'Post was successfully updated.') }
-      format.json  { head :no_content }
-    else
-      format.html  { render :action => "edit" }
-      format.json  { render :json => @dance_class.errors,
-                    :status => :unprocessable_entity }
+    respond_to do |format|
+      if @dance_class.update_attributes(params[:dance_class])
+        format.html  { redirect_to(@dance_class,
+                      :notice => 'Dance Class was successfully updated.') }
+        format.json  { head :no_content }
+      else
+        format.html  { render :action => "edit" }
+        format.json  { render :json => @dance_class.errors,
+                      :status => :unprocessable_entity }
+      end
     end
   end
-end
 
-def show
-  @dance_class = DanceClass.find(params[:id])
+  def show
+    @dance_class = DanceClass.find(params[:id])
+    @category = @dance_class.category
+    @dance_classes = DanceClass.find_all_by_category(@category)
+    @all_dance_classes = DanceClass.all
+    @tab = '[href="#tab' + params[:id] + '"]'
  
-  respond_to do |format|
-    format.html  # show.html.erb
-    format.json  { render :json => @dance_class }
+    respond_to do |format|
+      format.html  # show.html.erb
+      format.json  { render :json => @dance_class }
+    end
   end
 end
