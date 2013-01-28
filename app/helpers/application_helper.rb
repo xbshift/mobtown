3b1303030f2c7f9037f227bae3752ed4bfdcb456
Dan Allan <daniel.b.allan@gmail.com>
2013-01-28 15:11:17 -0500

    credited image tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
     not request.authorization.nil?
   end
 
+  def credited_image_tag (source, options={})
+    content_tag(:figure, class: options[:class]) do
+      image_tag(source, class: options[:class]) +
+      content_tag(:figcaption) do
+        render 'credit/' + options[:credit]
+      end
+    end
+  end
+
 end
