47a3200906f273bdd07a14c17d7559d826986525
Dan Allan <daniel.b.allan@gmail.com>
2013-02-03 14:51:51 -0500

    linked event pages, nice ckeditor images

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   attr_accessible :ends_at, :expiration, :prose, :special, :starts_at, :summary, :title, :photo
 
-  has_attached_file :photo, :styles => { :carousel => "700x450>",
+  has_attached_file :photo, :styles => { :carousel => "700x450#",
                                          :small => "300x" },
                     :url => "/assets/events/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/events/:id/:style/:basename.:extension"
