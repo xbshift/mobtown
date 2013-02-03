bc8ed1f87c49ff072c5c0a2d1a87f14ab3bb1e94
Dan Allan <daniel.b.allan@gmail.com>
2013-02-03 12:47:25 -0500

    carousel uses paperclip images and special events menu is database-backed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130202231139) do
+ActiveRecord::Schema.define(:version => 20130203165300) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -48,8 +48,12 @@ ActiveRecord::Schema.define(:version => 20130202231139) do
     t.boolean  "special"
     t.text     "summary"
     t.text     "prose"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
