7c9f5f45dbb919f75bb65bdb914bb2b1a3916007
Dan Allan <daniel.b.allan@gmail.com>
2013-02-10 13:29:37 -0500

    built twitter-bootstrap-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery-ui
 //= require jquery.ui.all
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require jquery-ui-sliderAccess
 //= require jquery-ui-timepicker-addon
 //= require_tree .
