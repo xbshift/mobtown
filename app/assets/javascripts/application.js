714c65bee46334b6a58da3c311fe3b52e062f1fe
Dan Allan <daniel.b.allan@gmail.com>
2013-02-18 23:02:33 -0500

    simple form classes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery-ui
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require bootstrap-datepicker
 //= require jquery-ui-sliderAccess
 //= require jquery-ui-timepicker-addon
 //= require ckeditor/init
