e22059d05961d108fb562fe282cc260c3ff80eda
Dan Allan <daniel.b.allan@gmail.com>
2013-03-22 17:44:56 -0400

    Create new categories in line on New Class form.

--- app/models/dance_class.rb
+++ app/models/dance_class.rb
@@ -1,8 +1,15 @@
 class DanceClass < ActiveRecord::Base
-  attr_accessible :name, :description, :category, :price, :student_price, :session_dates, :category_id
+  attr_accessible :name, :description, :category, :price, :student_price, :session_dates, :category_id, :new_category_name
   extend FriendlyId
   friendly_id :name, use: :slugged
 
   belongs_to :category
+
+  attr_accessor :new_category_name
+  before_save :create_category_from_name
+
+  def create_category_from_name
+    create_category(name: new_category_name) unless new_category_name.blank?
+  end
 
 end
