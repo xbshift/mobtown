317eacc5fbf0496b423dfab46315189e068ea0d0
Dan Allan <daniel.b.allan@gmail.com>
2013-05-06 17:19:03 -0400

    Single-option Stripe payments for Events.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,24 +1,24 @@
 class ChargesController < ApplicationController
 
-def create
-  # Amount in cents
-  @amount = 500
+  def new
+  end
 
-  customer = Stripe::Customer.create(
-    :email => params[:email],
-    :card  => params[:stripeToken]
-  )
+  def create
+    @amount = (params[:amount].to_f*100).to_i
+ 
+    customer = Stripe::Customer.create(
+      :email => 'example@stripe.com',
+      :card  => params[:stripeToken]
+    )
 
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => 6000
-    :description => 'Rails Stripe customer',
-    :currency    => 'usd'
-  )
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => params[:registrant_name] + ' ' + params[:registrant_email],
+      :currency    => 'usd'
+    )
 
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to charges_path
-end
-
-end
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+    end
+  end
