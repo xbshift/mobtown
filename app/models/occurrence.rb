e242591e7302b7fa9e29a96f4bd4e641368fe75d
Dan Allan <daniel.b.allan@gmail.com>
2013-05-06 22:55:16 -0400

    BUG: Refine Occurrence scopes that select date ranges.

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -2,11 +2,11 @@ class Occurrence < ActiveRecord::Base
   attr_accessible :end, :start, :event_id
   belongs_to :event
   
-  scope :tonight, where('start between ? and ?', Date.today.beginning_of_day, Date.today.end_of_day).order(:start)
+  scope :tonight, where('start = ?', Date.today).order(:start)
 
-  scope :this_week, where('start between ? and ?', Date.today, Date.today.end_of_week).order(:start)
+  scope :this_week, where('start between ? and ?', Date.today + 1.day, [Date.today + 1.day, Date.today.end_of_week].min).order(:start)
 
-  scope :next_week, where('start between ? and ?', Date.today.end_of_week, Date.today.end_of_week + 7.days).order(:start)
+  scope :next_week, where('start between ? and ?', Date.today.end_of_week + 1.day, Date.today.end_of_week + 7.days).order(:start)
 
   scope :next, lambda { |n| where('start > ?', Date.today).order(:start).limit(n) }
 
